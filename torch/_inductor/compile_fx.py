389859b44427d551f7bcc778096bcd8e4f63057b
Elias Ellison <elias.ellison@gmail.com>
2023-06-06 01:09:14 +0000

    Fold Conv-Bn
    ghstack-source-id: 3152e7200a62d8318b1df4b242c260c3b287df84
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/100653

--- torch/_inductor/compile_fx.py
+++ torch/_inductor/compile_fx.py
@@ -708,6 +708,10 @@ def compile_fx(
 
     graph_id = next(_graph_counter)
 
+    decompositions = (
+        decompositions if decompositions is not None else select_decomp_table()
+    )
+
     @dynamo_utils.dynamo_timed
     def fw_compiler_base(model: torch.fx.GraphModule, example_inputs, is_inference):
         if is_inference:
@@ -781,8 +785,12 @@ def compile_fx(
 
     fw_compiler = functools.partial(fw_compiler_base, is_inference=False)
 
-    if config.freezing:
+    if config.freezing and not torch.is_grad_enabled():
         from torch._inductor.freezing import freeze
+
+        # Dont want to decompose BN so we can fold Conv-BN
+        decompositions = dict(decompositions)
+        del decompositions[torch.ops.aten._native_batch_norm_legit_no_training.default]
 
         def inference_compiler(
             aot_autograd_model: torch.fx.GraphModule, example_inputs
@@ -793,7 +801,7 @@ def compile_fx(
             opt_model, preserved_arg_indices = freeze(
                 model_,
                 aot_autograd_model,
-                fw_metadata=torch._guards.TracingContext.get().fw_metadata,
+                example_inputs,
             )
 
             example_inputs = [example_inputs[ind] for ind in preserved_arg_indices]
@@ -855,8 +863,6 @@ def compile_fx(
                 boxed_forward_device_index=forward_device,
             )
 
-    if decompositions is None:
-        decompositions = select_decomp_table()
     # TODO: can add logging before/after the call to create_aot_dispatcher_function
     # in torch._functorch/aot_autograd.py::aot_module_simplified::aot_function_simplified::new_func
     # once torchdynamo is merged into pytorch
